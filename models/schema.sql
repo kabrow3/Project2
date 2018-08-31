0c62e9026c92ad9ce9373ea5638e2185afed28b9
shellyrgarner <sgrenea@gmail.com>
2018-08-30 20:49:01 -0400

    added rideshare db and ride table

--- models/schema.sql
+++ models/schema.sql
@@ -10,3 +10,22 @@ CREATE TABLE IF NOT EXISTS events_db.events
   PRIMARY KEY(event_id)
 );
 
+
+
+DROP DATABASE IF EXISTS rideshare_db;
+CREATE DATABASE rideshare_db;
+
+CREATE TABLE IF NOT EXISTS rideshare_db.ride
+(
+  riderId INT NOT NULL,
+  driverId INT NOT NULL,
+  email VARCHAR(50) NULL ,
+  confirmed TINYINT NULL DEFAULT 0,
+  startLocation VARCHAR(50) NULL,
+  endLocation VARCHAR(50) NULL,
+  numSeats INT NULL DEFAULT 0,
+  departureTime VARCHAR(50) NULL,
+  created TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (riderId) 
+);
+
