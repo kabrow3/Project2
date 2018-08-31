2054c94987db6d1d00de106590a910807bfceef9
Denholm <jamesmeier94@gmail.com>
2018-08-30 21:27:56 -0400

    authentication

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.User.findAll({}).then(function(dbUser) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        users: dbUser
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/users/:id", function(req, res) {
+    db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
       res.render("example", {
-        example: dbExample
+        users: dbUser
       });
     });
   });
