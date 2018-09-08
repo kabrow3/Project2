4dd1215b192d958489abed5414ec216bc343fa44
Denholm <jamesmeier94@gmail.com>
2018-09-08 01:44:01 -0400

    L

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,10 @@
 // No longer need this file, technically, since the forms function as intended.
+$("#daily").click(function() {
+    $("#cardDaily").toggle();
+});
+$("#weekly").click(function() {
+    $("#cardWeekly").toggle();
+});
+$("#monthly").click(function() {
+    $("#cardMonthly").toggle();
+});
